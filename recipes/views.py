770e6b7bf42d7853e391e828953e1183b0660cb6
Victor de Souto <victor.souto@hotmail.com>
2024-02-10 12:40:09 -0300

    feat: home

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Victor Souto'
     })
-
-
-def sobre(request):
-    return HttpResponse('HEROU WUROLDE')
-
-
-def contato(request):
-    return render(request, 'temp/temp.html')
